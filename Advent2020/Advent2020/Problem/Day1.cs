e6fda2f0cf4d5ad357eb541aa56c39e7c4f50d77
Daniel Månsson <daniel.mansson@rovio.com>
2020-12-01 07:42:08 +0100

    Day 1

--- Advent2020/Advent2020/Problem/Day1.cs
+++ Advent2020/Advent2020/Problem/Day1.cs
@@ -10,16 +10,35 @@ namespace Advent2020.Problem
     public class Day1 : BaseDay
     {
         public override string Example1 => 
-            @"";
+            @"1721
+979
+366
+299
+675
+1456";
 
-        string Transform(string raw)
+        List<int> Transform(string raw)
         {
-            return raw;
+            return raw
+                .Split("\n")
+                .Select(l => int.Parse(l))
+                .ToList();
         }
 
         public override object Solve1(string raw)
         {
             var input = Transform(raw);
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = i; j < input.Count; j++)
+                {
+                    if (input[i] + input[j] == 2020)
+                    {
+                        return input[i] * input[j];
+                    }
+                }
+            }
 
             return -1;
         }
@@ -27,6 +46,20 @@ namespace Advent2020.Problem
         public override object Solve2(string raw)
         {
             var input = Transform(raw);
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = i; j < input.Count; j++)
+                {
+                    for (int k = i; k < input.Count; k++)
+                    {
+                        if (input[i] + input[j] + input[k] == 2020)
+                        {
+                            return input[i] * input[j] * input[k];
+                        }
+                    }
+                }
+            }
 
             return -1;
         }
